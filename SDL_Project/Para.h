a2e609da80f0aaa4f99bf6df68914d3f820a6bcc
trananh91 <tranducanh717@gmail.com>
2022-05-16 23:59:37 +0700

    Update Para.h

--- SDL_Project/Para.h
+++ SDL_Project/Para.h
@@ -5,14 +5,22 @@
 
 using namespace std;
 
-const int SCREEN_WIDTH = 800;
+const int SCREEN_WIDTH = 1400;
 const int SCREEN_HEIGHT = 800;
-const string WINDOW_TITLE = "TEST BACKGROUND";
+const string WINDOW_TITLE = "MY WAY TO VNU";
+
+const int LeftEdge = 190;
+const int RightEdge = 1100;
+
+const int UpEdge = 0;
+const int DownEdge = 700;
+
 const Uint32 StartTime = 0;
 
 static int Score = 0;
+static int numberOfBullet = 3;
 
-const int NumberOfBoxes = 10;
-const int BoxesPerRow = 4;
+const int NumberOfViruses = 100;
+const int VirusesPerRow = 5;
 
 #endif // PARA__H
