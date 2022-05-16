f3f9abe468d8fe63cff7c85f52aaa9a500f6187a
trananh91 <tranducanh717@gmail.com>
2022-05-16 23:58:55 +0700

    Update main.cpp

--- SDL_Project/main.cpp
+++ SDL_Project/main.cpp
@@ -7,7 +7,7 @@
 #include "Box.h"
 #include "game.h"
 #include "Character.h"
-#include "BackGround.h"
+#include "Bullet.h"
 #include <stdio.h>
 #include <SDL_ttf.h>
 
@@ -19,102 +19,59 @@ const Uint32 frameDelay = 1000 / FPS;
 
 int main(int argc, char* argv[])
 {
+    // Init SDL
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
-
     initSDL(window, renderer);
-
-    SDL_Texture* background;
 
     // Font
     TTF_Init();
-    TTF_Font* font = TTF_OpenFont("digital-7.ttf", 100);
+    TTF_Font* font = TTF_OpenFont("waltograph42.ttf", 90);
 
-    SDL_Rect Background = {100, 100, SCREEN_WIDTH/2, SCREEN_HEIGHT/2 };
-    background = loadTexture("universe1.jpg", renderer);
+    //Background
+    SDL_Texture* background = loadTexture("School.jpg", renderer);
 
+    // srand for random
     srand(time(nullptr));
 
     Game game(renderer);
-    game.CountDown(Background, font, background);
+    game.CountDown(font, background);
     game.LoadBoxes();
-//    background = loadTexture("universe.jpg", renderer);
+
+    // Change background
     background = loadTexture("Road.png", renderer);
 
-    int counter = 0;
-//    while(!game.Over(font))
-    while (true)
+    while (!game.Over(background))
     {
-        game.checkBoxDes();
-
         Uint32 TimeStart = SDL_GetTicks();
+
+        // Load background
         game.loadBackGround(background, renderer);
 
-        SDL_RenderCopy(renderer, background, NULL, NULL);
+        // Draw characters
+        game.GameDraw();
 
-        game.updateBox();
+        // Update Virus and Bullet
+        game.Update();
 
+        // Event for keyboard
         SDL_Event e;
-        if (SDL_PollEvent(&e) != 0){
-        if (e.type == SDL_KEYDOWN)
-        {
-            switch (e.key.keysym.sym)
-            {
-//                case SDLK_ESCAPE: break;
-                case SDLK_RIGHT:
-                    game.updateRight();
-                    break;
-                    continue;
-                case SDLK_LEFT:
-                    game.updateLeft();
-                    break;
-                    continue;
-                case SDLK_UP:
-                    game.updateUp();
-                    break;
-                    continue;
-                case SDLK_DOWN:
-                    game.updateDown();
-                    break;
-                    continue;
-                default:
-                    game.updateBox();
+        game.handleEvent(e);
 
-                    continue;
-            }
-        }
-    }
-        counter++;
-//        game.draw();
-        game.GameTestDraw();
+        // Counting % to win and Score
+        game.GameCounting(font);
+        game.checkScoreAdded();
 
         SDL_RenderPresent(renderer);
-
         SDL_RenderClear(renderer);
+
         Uint32 end = SDL_GetTicks() - TimeStart;
         if (frameDelay > end)
         {
             SDL_Delay(frameDelay - end);
         }
-//        if (game.checkCollision1() || Score == NumberOfBoxes)
-//        {
-//            game.Over1(font);
-//            break;
-//        }
-
-        if (game.Over1(font, background))
-        {
-            break;
-        }
     }
-
-//    SDL_RenderClear(renderer);
-//    game.loadBackGround(background, renderer);
-//    game.OverWin();
-//    game.SpaceShip.draw(renderer);
-//    SDL_RenderPresent(renderer);
-
-    SDL_Delay(6000);
+    SDL_Delay(4000);
 
     SDL_DestroyTexture (background);
     background = NULL;
